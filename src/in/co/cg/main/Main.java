cd7c6f2677ca510b0365d434d47870384a663725
rnaik469 <rnaik469@gmail.com>
2018-08-07 20:42:57 +0530

    Update Main.java

--- src/in/co/cg/main/Main.java
+++ src/in/co/cg/main/Main.java
@@ -1,10 +1,9 @@
 package in.co.cg.main;
-
 import in.co.cg.ui.UI;
 
 public class Main {
 	public static void main(String[] args) {
-		UI ui =new UI();
-		ui.acceptData();
+		UI ui =new UI(); // Instantiating UI class
+		ui.acceptDataAndOperations(); // Invoking acceptDataAndOperations of UI class
 	}
 }
